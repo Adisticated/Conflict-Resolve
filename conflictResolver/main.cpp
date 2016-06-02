f97551ebb78202efc5891cd5217b206c5312e41b
aditya <aditya@aditya.corp.aptportfolio.com>
2016-06-02 11:29:12 +0530

    Aditya: Removing comments

---
+++ conflictResolver/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: aditya
+ *
+ * Created on 2 June, 2016, 11:26 AM
+ */
+
+#include <cstdlib>
+#include<iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    std::cout<<"hello";
+    return 0;
+}
+
